f06b75619a148c7a2de33c8e5780bebde05c30b2
Alex <angrylexa@mail.ru>
2022-08-25 14:24:54 +0300

    DZ38

--- Program.cs
+++ Program.cs
@@ -62,3 +62,36 @@
 //     sum +=array[i];
 // }
 // Console.Write($"Сумма элементов , стоящих  на нечетных позициях равна {sum}");
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+// Console.Clear();
+// int[] FillArray(int[] array, int min, int max)
+// {
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = rnd.Next(min, max = 1);
+//     }
+//     return array;
+// }
+// int[] array = new int[5];
+// FillArray(array, -10, 10);
+// Console.WriteLine($"[{String.Join(" , ", array)}]");
+// int min = array[0];
+// int max = array[0];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] > max)
+//     {
+//         max = array[i];
+//     }
+//     else if (array[i] < min)
+//     {
+//         min = array[i];
+//     }
+// }
+// Console.Write($"Разница между максимальным и минимальным элементом массива равна {max - min}");
