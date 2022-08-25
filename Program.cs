7e185cadb41fb14779d2775d94b9a7e1ca229f9c
Alex <angrylexa@mail.ru>
2022-08-25 14:11:11 +0300

    DZ36

--- Program.cs
+++ Program.cs
@@ -36,3 +36,29 @@
 //     }
 // }
 // Console.Write($"[{String.Join(", ", array) }] количество четных {count}");
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+// Console.Clear();
+// int[] FillArray(int[] array, int min, int max)
+// {
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = rnd.Next(min, max = 1);
+//     }
+//     return array;
+// }
+// int[] array = new int[4];
+// FillArray(array, -10, 10);
+// Console.WriteLine($"[{String.Join(" , ", array)}]");
+// int sum = 0 ;
+// for (int i = 1; i < array.Length; i+= 2)
+// {
+//     sum +=array[i];
+// }
+// Console.Write($"Сумма элементов , стоящих  на нечетных позициях равна {sum}");
